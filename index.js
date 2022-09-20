168f9a7ab49fb8b7d0a2c6f55f2e03e6464240f0
Sebastian Fritzsche <sebifr58@gmx.de~>
2022-09-20 21:18:28 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const CLIENT_ID = '120ed296515f452dba22e78c5757d84b';
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+const REDIRECT_URI = 'http://127.0.0.1:5500/home.html';
+const RESPONSE_TYPE = 'token';
